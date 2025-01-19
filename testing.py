bc05dda5a28b44c16dec15ee92fa88a0cd100df0
hrishikesh patil <hrishipattil193@gmail.com>
2025-01-19 10:45:42 +0530

    added file to testing files

---
+++ testing.py
@@ -0,0 +1,10 @@
+print("hello hrishikesh")
+
+
+# git init means = initialzing a empty respositry after which we can see .git file
+# git status = it shows the information into the git folder.
+# git add file name = it adds file to the staged part.
+# git rm --cached testing.py means it has been put in the untraced part.
+# git commit -m"any message" = it saves the changes and the file is put into the traced form now
+# git restore testing.py = it gets back the deleted file only if it is into the vcs. 
+# 
